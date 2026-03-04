dc87a3c4def9ad7e55bc760af644a60f417dd5d3
koweteagle <koweteagle@gmail.com>
2026-03-04 20:12:15 +0100

    a

--- game.js
+++ game.js
@@ -745,7 +745,7 @@ function render() {
         ctx.save();
     
         // Teken rond het midden van de sprite
-        const hitYOffset = (t.isHit && (t.variant === 1 || t.variant === 3)) ? 65 : (t.isHit ? 28 : 0);  // normale supporters op de grond op stoep
+        const hitYOffset = (t.isHit && t.variant === 3) ? 95 : (t.isHit && t.variant === 1) ? 65 : (t.isHit ? 28 : 0);  // supA/supC op stoep, supC lager
         ctx.translate(t.x + halfW, VIRTUAL_HEIGHT - 50 + hitYOffset);
     
         // Spiegelen
