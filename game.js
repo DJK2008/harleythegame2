d7b7815605830a4231bf18567ff499b4ee26c470
koweteagle <koweteagle@gmail.com>
2026-03-05 17:52:03 +0100

    a scale4

--- game.js
+++ game.js
@@ -273,12 +273,12 @@ const SUP_C_KEYS = ['supC1', 'supC2', 'supC3'];
 const SUP_D_KEYS = ['groen1', 'groen2', 'groen3', 'groen4', 'groen5', 'groen6', 'groen7', 'groen8', 'groen9'];
 
 // --- Grootte/schaal per type (1 = standaard, <1 kleiner, >1 groter) ---
-const SUP_A_SCALE_X = 3;
+const SUP_A_SCALE_X = 4;
 const SUP_A_SCALE_Y = 1.9;
 const SUP_C_SCALE_X = 1;
 const SUP_C_SCALE_Y = 1;
 const SUP_D_SCALE_X = 1.50;
-const SUP_D_SCALE_Y = 1.05;
+const SUP_D_SCALE_Y = 1.05; 
 const HOOLI_SCALE_X = 1;
 const HOOLI_SCALE_Y = 1;
 const BOSS_SCALES = { boss1: 1, boss2: 1, boss3: 1, boss4: 1 };
